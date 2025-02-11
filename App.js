98d770108c088b2362b3a598687c51cb75be5724
venkat265 <venkatganeshmaddineni@gmail.com>
2025-02-11 10:16:01 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+// import React from 'react';
+
+// //counter component
+
+// const Counter=()=>{
+//   const [count,setCount]=React.useState(0);
+//   //current state count
+//   //use state() to intialize
+//   return(
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={()=>setCount(count+1)}>Increment</button>
+//       <button onClick={()=>setCount(count-1)}>Decrement</button>
+//     </div>
+//   )
+// }
+
+// export default Counter;
+import React, { useState } from "react";
 
 //counter component
 
-const Counter=()=>{
-  const [count,setCount]=React.useState(0);
-  //current state count
-  //use state() to intialize
+const ThemeToggler=()=>{
+  const [theme,setTheme] = useState('light')
+
+  const toggleTheme = () =>{
+    setTheme(prevTheme=>(prevTheme == 'light' ? 'dark' : 'light'))
+  }
+
   return(
-    <div>
-      <h1>{count}</h1>
-      <button onClick={()=>setCount(count+1)}>Increment</button>
-      <button onClick={()=>setCount(count-1)}>Decrement</button>
+    <div style={{backgroundColor:theme=='light'?'white':'black' , color:theme=='light'? 'black':'white' ,textAlign:'center'}}>
+      <h1>The Current theme is {theme}</h1>
+      <button onClick={toggleTheme}>toggleTheme</button>
     </div>
   )
-}
 
-export default Counter;
+}
+export default ThemeToggler;
